2f01c1f0277d0f057031dceb2bd59bc99ef0f96f
Andre Schuelein <andreschuelein@gmail.com>
2019-05-31 00:10:59 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM python:3.8-rc-stretch
+
+RUN apt-get update && apt-get install -y \
+    zlib1g-dev \
+    libjpeg-dev \
+    python3-pythonmagick \
+    inkscape \
+    xvfb \
+    poppler-utils \
+    libfile-mimeinfo-perl \
+    qpdf \
+    libreoffice \
+    && wget https://sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-11.11.tar.gz \
+    && gzip -dc Image-ExifTool-11.11.tar.gz | tar -xf - \
+    && cd Image-ExifTool-11.11 \
+    && perl Makefile.PL \
+    && make install \
+    && rm -f ../Image-ExifTool-11.11.tar.gz \
+    && pip install preview-generator
+
+WORKDIR /var/app
---
+++ app/preview.py
@@ -0,0 +1,47 @@
+from preview_generator.manager import PreviewManager
+import time
+
+
+cache_path = '/var/preview-cache'
+source_document = '/var/data/MARBLES.TIF'
+
+manager = PreviewManager('/var/preview-cache/', create_folder=True)
+# start_time = time.time()
+# for x in range(0, 500):
+#     preview_path = manager.get_jpeg_preview(
+#         source_document, height=1000, width=1000, force=True)
+#     if x % 10 == 0:
+#         print("--- %s seconds ---" % (time.time() - start_time))
+#         start_time = time.time()
+
+# preview_path = manager.get_pdf_preview(
+# source_document, force=True)
+preview_path = manager.get_jpeg_preview(
+    source_document, height=1000, width=1000, force=True)
+
+# has_pdf_preview
+# get_pdf_preview
+
+# has_jpeg_preview
+# get_jpeg_preview
+# get_jpeg_preview(source_document,height=100,width=50)
+
+# has_text_preview
+# get_text_preview
+
+# has_json_preview
+# get_json_preview
+
+# has_html_preview
+# get_html_preview
+
+# get_page_nb
+# get_mimetype
+print('')
+print('original document : ', source_document)
+print('has_pdf_preview  : ', manager.has_pdf_preview(source_document))
+print('has_jpeg_preview : ', manager.has_jpeg_preview(source_document))
+print('has_text_preview : ', manager.has_text_preview(source_document))
+print('has_json_preview : ', manager.has_json_preview(source_document))
+print('has_html_preview : ', manager.has_html_preview(source_document))
+print('Preview created at path : ', preview_path)
